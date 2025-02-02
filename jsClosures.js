f196b9090aa0fb3c7a8df227823a15cc6797ea12
ricardogk3 <rgk19@hotmail.com>
2025-02-02 17:46:47 -0300

    criando um closure com um criador de funçoes

--- jsClosures.js
+++ jsClosures.js
@@ -25,3 +25,18 @@ console.log(copiafun.reduz())
 console.log(copiafun.reduz())
 console.log(copiafun.reduz())
 
+
+
+//criador de funçoes
+
+function makeAdder(x) {
+    return function (y) {
+      return x + y;
+    };
+  }
+  
+  var add5 = makeAdder(5);
+  var add10 = makeAdder(10);
+  
+  print(add5(2)); // 7
+  print(add10(2)); // 12
